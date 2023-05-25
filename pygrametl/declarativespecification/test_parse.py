f6e255b3395e9e8df1c1be2a2906362835dab621
Simon Mathiasen <33898979+smath17@users.noreply.github.com>
2023-05-25 20:22:53 +0200

    header + generated example

--- pygrametl/declarativespecification/test_parse.py
+++ pygrametl/declarativespecification/test_parse.py
@@ -5,7 +5,7 @@ from pygrametl.declarativespecification.parsing import IntermediateSpecification
 def main():
     spec = IntermediateSpecification('test-toml.txt')
     generator = PygramGenerator(spec)
-    generator.create_pygram_file()
+    generator.create_pygram_file(spec)
 
 
 if __name__ == "__main__":
