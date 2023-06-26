3541c61710396b9a019961898ed21e1d33e60709
Simon Mathiasen <33898979+smath17@users.noreply.github.com>
2023-06-26 20:10:27 +0200

    test uses package
    group name is fixed

--- pygrametl/declarativeetl/test_parse.py
+++ pygrametl/declarativeetl/test_parse.py
@@ -3,23 +3,9 @@ import sys
 from pygrametl.declarativeetl.DDLGenerator import DDLGenerator
 from pygrametl.declarativeetl.PygrametlGenerator import PygramGenerator
 from pygrametl.declarativeetl.parsing import IntermediateSpecification
-
-
-def create(spec_filepath):
-    spec = IntermediateSpecification(spec_filepath)
-    generate_pygram(spec)
-    generate_ddl(spec)
-
-
-def generate_pygram(spec):
-    generator = PygramGenerator()
-    generator.create_pygrametl_file(spec)
-
-
-def generate_ddl(spec):
-    ddl_generator = DDLGenerator(spec)
-    ddl_generator.make_ddl_file()
+from pygrametl.declarativeetl import create_source_files
 
 
 if __name__ == "__main__":
-    create(sys.argv[1])
+    # create(sys.argv[1])
+    create_source_files(sys.argv[1])
