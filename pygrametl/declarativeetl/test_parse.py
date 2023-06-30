1d9dcd0e51339aa19d7e21e08b40322b5d18e324
Simon Mathiasen <33898979+smath17@users.noreply.github.com>
2023-06-30 12:37:54 +0200

    Update test_parse.py

--- pygrametl/declarativeetl/test_parse.py
+++ pygrametl/declarativeetl/test_parse.py
@@ -1,11 +1,6 @@
 import sys
-
-from pygrametl.declarativeetl.DDLGenerator import DDLGenerator
-from pygrametl.declarativeetl.PygrametlGenerator import PygramGenerator
-from pygrametl.declarativeetl.parsing import IntermediateSpecification
 from pygrametl.declarativeetl import create_source_files
 
 
 if __name__ == "__main__":
-    # create(sys.argv[1])
     create_source_files(sys.argv[1])
